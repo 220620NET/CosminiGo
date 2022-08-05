417e0eaa9e216c75e71498e864547cdc94db3ad8
unknown <syunoncasual@gmail.com>
2022-08-05 01:04:44 -0400

    Finished stuff

--- main.js
+++ main.js
@@ -1,60 +1,123 @@
 $(document).ready(() => {
 
-
+let LeftSelect = false; //this get toggled in the RNG
+let RightSelect = false;
+let WinCondition = 0;
+let PlayCount = 0;
+let WinStreak = 0;
 $('.generate').click(function()
 {
-	/*fetch('https://api.agify.io/?name=michael',{ mode: 'no-cors'}).then((response) => response.text()).then(imageBlob => {
-      // Then create a local URL for that image and print it 
-      //const imageObjectURL = URL.createObjectURL(imageBlob);*/
-      let topImg = document.createElement("img"); //creates an html img tag
-      topImg.src = `https://app.pixelencounter.com/api/basic/stars?height=300&width=300&date=${Date.now()}`;
-      $('.tm').html(topImg);
-
-      let rightImg = document.createElement("img"); //creates an html img tag
-      rightImg.src = `https://app.pixelencounter.com/api/v2/basic/svgmonsters?date=${Date.now()}`;
-      $('.right').html(rightImg);
-
-      let bottomImg = document.createElement("img"); //creates an html img tag
-      bottomImg.src = `https://app.pixelencounter.com/api/basic/planets?frame=450&height=300&width=300&date=${Date.now()}`;
-      $('.bm').html(bottomImg);
-
-      let leftImg = document.createElement("img"); //creates an html img tag
-      leftImg.src = `https://app.pixelencounter.com/api/basic/monsters/random?date=${Date.now()}`;
-      $('.left').html(leftImg);
-      //console.log(imageBlob);
-  //});
-		//{
-		 //console.log(resBody);
-    	//document.querySelector('#result-name').innerText = resBody.name;
-        //document.getElementById('result-age').innerText = resBody.age;
-        //}).catch((err) => console.log(err));
-
-        /*const reqArr = ['https://api.agify.io/?name=' + nameInput, 'https://api.genderize.io?name=' + nameInput, 'https://api.nationalize.io?name=' + nameInput];
-        // Promise.all([fetch(reqArr[0]).then((res) => res.json()), fetch(reqArr[1]).then((res) => res.json()), fetch(reqArr[2]).then((res) => res.json())])
+    $('.Player-counter-display').text(PlayCount);
+    PlayCount++;
+        let WinCondition = Math.floor(Math.random() * 2);
+        if ((WinCondition === 0 && LeftSelect === true) || (WinCondition === 1 && RightSelect === true))
+        {
+            console.log(WinStreak);
+            WinStreak = WinStreak + 1;
+            $('.Winning_streak_display').text(WinStreak);
+            LeftSelect = false;
+            LeftSelect = false;
+            $('.image1').removeClass('colour');
+            $('.image4').removeClass('colour');
+            alert("You have won purely by luck, zero skill involved");
+        }
+        else if(LeftSelect === false && RightSelect === false)
+        {
+            console.log(WinStreak);
+            WinStreak = 0;
+            $('.Winning_streak_display').text(WinStreak);
+            LeftSelect = false;
+            LeftSelect = false;
+            $('.image1').removeClass('colour');
+            $('.image4').removeClass('colour');
+            alert("Are you even trying?")
+        }
+        else
+        {
+            console.log(WinStreak);
+            WinStreak = 0;
+            $('.Winning_streak_display').text(WinStreak);
+            LeftSelect = false;
+            LeftSelect = false;
+            $('.image1').removeClass('colour');
+            $('.image4').removeClass('colour');
+            alert("You stink")
+        }
         
-        //refactoring the above long Promise.all call to utilize js's Array.map() method (this is akin to C# array's foreach method or LINQ's select method)
-        Promise.all(reqArr.map((url) => fetch(url).then((res) => res.json()))).then((data) => {
-            console.log(data);
-            //format the data
-            responseData = {
-                name: data[1].name,
-                gender: {
-                    gender: data[1].gender,
-                    probability: data[1].probability
-                },
-                age: data[0].age,
-                nationality: {
-                    country: data[2].country[0]?.country_id,
-                    probability: data[2].country[0]?.probability
-                }
-            };
+        let topImg = document.createElement("img"); //creates an html img tag
+        topImg.src = `https://app.pixelencounter.com/api/basic/stars?height=300&width=300&date=${Date.now()}`;
+        $('.tm').html(topImg);
 
-            callback(responseData);
-        }) 
-    }*/
+        let rightImg = document.createElement("img"); //creates an html img tag
+        rightImg.src = `https://app.pixelencounter.com/api/v2/basic/svgmonsters?date=${Date.now()}`;
+        $('.image4').html(rightImg);
 
+        let bottomImg = document.createElement("img"); //creates an html img tag
+        bottomImg.src = `https://app.pixelencounter.com/api/basic/planets?frame=450&height=300&width=300&date=${Date.now()}`;
+        $('.bm').html(bottomImg);
 
+        let leftImg = document.createElement("img"); //creates an html img tag
+        leftImg.src = `https://app.pixelencounter.com/api/basic/monsters/random?date=${Date.now()}`;
+        $('.image1').html(leftImg);
+ 
+});
 
+$('.SelectLeft').click(function()
+{
+    if(LeftSelect===true && RightSelect===false)
+    {
+        LeftSelect = false;
+        $('.image1').removeClass('colour');
+    }
+    else if(LeftSelect===true && RightSelect===true)
+    {
+        $('.image1').removeClass('colour');
+        $('.image4').removeClass('colour');
+        LeftSelect = false;
+        RightSelect = false;
+        alert("How TF you got here m8");
+    }
+    else if(LeftSelect===false && RightSelect===false)
+    {
+        LeftSelect = true;
+        $('.image1').addClass('colour');
+    }
+    else if(LeftSelect===false && RightSelect===true)
+    {
+        LeftSelect = true;
+        $('.image1').addClass('colour');
+        RightSelect = false;
+        $('.image4').removeClass('colour');
+    }
+});
+
+$('.SelectRight').click(function()
+{
+    if(LeftSelect===true && RightSelect===false)
+    {
+        LeftSelect = false;
+        $('.image1').removeClass('colour');
+        RightSelect = true;
+        $('.image4').addClass('colour');
+    }
+    else if(LeftSelect===true && RightSelect===true)
+    {
+        $('.image1').removeClass('colour');
+        $('.image4').removeClass('colour');
+        LeftSelect = false;
+        RightSelect = false;
+        alert("How TF you got here m8");
+    }
+    else if(LeftSelect===false && RightSelect===false)
+    {
+        RightSelect = true;
+        $('.image4').addClass('colour');
+    }
+    else if(LeftSelect===false && RightSelect===true)
+    {
+        RightSelect = false;
+        $('.image4').removeClass('colour');
+    }
 });
 });
 	
